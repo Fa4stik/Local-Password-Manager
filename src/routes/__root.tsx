506f8600b316759a15bbdfc1667f677ceadb4165
Fa4stik <stepan.sharifulin@mail.ru>
2024-11-13 20:21:40 +0500

    Update - Hide Tanstackrouter for production

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,19 @@
 import { createRootRoute, Outlet, useNavigate } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import { usePasswordsStore } from "../store/usePasswordsStore.ts";
 import { decryptData, encryptData } from "../lib/cryptData.ts";
+
+const TanStackRouterDevtools =
+	import.meta.env.MODE === 'production'
+		? () => null // Render nothing in production
+		: React.lazy(() =>
+			// Lazy load in development
+			import('@tanstack/router-devtools').then((res) => ({
+				default: res.TanStackRouterDevtools,
+				// For Embedded Mode
+				// default: res.TanStackRouterDevtoolsPanel
+			})),
+		)
 
 export const Route = createRootRoute({
 	component: RootPage
@@ -55,6 +66,8 @@ function RootPage () {
 
 	return <>
 		<Outlet />
-		<TanStackRouterDevtools />
+		<Suspense>
+			<TanStackRouterDevtools />
+		</Suspense>
 	</>
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-// import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    // TanStackRouterVite()
+    TanStackRouterVite()
   ],
   server: {
     host: '0.0.0.0',
